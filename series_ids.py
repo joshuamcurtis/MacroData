dc121eea0420a095347a80ab50cbdd690be362ea
unknown <joshua.mi.curtis@gmail.com>
2022-09-26 20:51:40 -0400

    Created a file with all the CPI series IDs and some reference data.

---
+++ series_ids.py
@@ -0,0 +1,20 @@
+# CUSR Consumer Price Index - All Urban Consumers - Seasonally adjusted - Monthly
+
+CPI = ["CUUR0000SA0", # CPI for All Urban Consumers (CPI-U), All items in U.S. city average, all urban consumers, not seasonally adjusted
+       "CUSR0000SA0", # CPI for All Urban Consumers (CPI-U), All items in U.S. city average, all urban consumers, seasonally adjusted
+       "CUSR0000SA0E", #Energy
+       "CUSR0000SA0L1", #All items less food
+       "CUSR0000SA0L12", #All items less food and shelter
+       "CUSR0000SA0L12E", #All items less food, shelter, and energy
+       "CUSR0000SA0L12E4", #All items less food, shelter, energy, and used cars and trucks
+       "CUSR0000SA0L1E", #All items less food and energy
+       "CUSR0000SA0L2", #All items less shelter
+       "CUSR0000SA0L5", #All items less medical care
+       "CUSR0000SA0LE", #All items less energy
+       "CUSR0000", #
+       "CUSR0000", #
+       "CUSR0000", #
+       "CUSR0000", #
+       "CUSR0000", #
+       "CUSR0000", #
+      ] 
